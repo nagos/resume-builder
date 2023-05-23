11adaea8630b202fd7ccc55a3b9a95c008ef34c7
Vladimir Yakovlev <nagos@inbox.ru>
2023-05-24 02:10:56 +0300

    Рефакторинг

--- backend/app.py
+++ backend/app.py
@@ -6,47 +6,46 @@ app = Flask(__name__)
 app.secret_key = os.getenv('API_KEY', 'dev key')
 db_server = os.getenv('DB_SERVER')
 
-def get_db_connection():
-    cnx = getattr(g, '_database', None)
-    if cnx is None:
-        cnx = g._database = connect(
+def get_db():
+    cnx = connect(
             host=db_server,
             user="root",
             password="",
             database="resume"
         )
-    return cnx
+    cursor = cnx.cursor()
+    return cnx, cursor
 
 @app.route('/api/login/register', methods=['POST'])
 def register():
-    cnx = get_db_connection()
+    cnx, cursor = get_db()
     try:
-        with cnx.cursor() as cursor:
-            create_user_query = "INSERT INTO users (login, password) VALUES (%s, %s)"
-            user = request.form.get('user')
-            password = request.form.get('password')
-            cursor.execute(create_user_query, (user, password))
-            session['login'] = True
-            print(user, password)
-            cnx.commit()
+        create_user_query = "INSERT INTO users (login, password) VALUES (%s, %s)"
+        user = request.form.get('user')
+        password = request.form.get('password')
+        cursor.execute(create_user_query, (user, password))
+        session['login'] = True
+        session['id'] = cursor.lastrowid
     except:
         return "fail", 400
+    cnx.commit()
+    cnx.close()
     return "ok"
 
 @app.route('/api/login/login', methods=['POST'])
 def login():
     user = request.form.get('user')
     password = request.form.get('password')
-    cnx = get_db_connection()
-    with cnx.cursor() as cursor:
-        user_password_query = "SELECT id, password FROM users WHERE login=%s"
-        cursor.execute(user_password_query, (user,))
-        user_id, user_password = cursor.fetchone()
-        if password == user_password:
-            session['login'] = True
-            session['id'] = user_id
-        else:
-            return "fail", 400
+    cnx, cursor = get_db()
+    user_password_query = "SELECT id, password FROM users WHERE login=%s"
+    cursor.execute(user_password_query, (user,))
+    user_id, user_password = cursor.fetchone()
+    if password == user_password:
+        session['login'] = True
+        session['id'] = user_id
+    else:
+        return "fail", 400
+    cnx.close()
     return "ok"
 
 @app.route('/api/login/logout', methods=['POST'])
