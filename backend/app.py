e161392411bbb841e1beec5c8c26acb26ab47992
Vladimir Yakovlev <nagos@inbox.ru>
2023-05-24 03:42:09 +0300

    Хранение резюме

--- backend/app.py
+++ backend/app.py
@@ -52,3 +52,84 @@ def login():
 def logout():
     session.clear()
     return "ok"
+
+@app.route('/api/resume/<int:id>')
+def resume_get(id):
+    cnx, cursor = get_db()
+    try:
+        resume_get_query = "SELECT text FROM resume WHERE id=%s"
+        cursor.execute(resume_get_query, (id,))
+        text, = cursor.fetchone()
+    except:
+        return "fail", 400
+    cnx.close()
+    return text
+
+@app.route('/api/resume/')
+def resume_list():
+    if not session.get('login'):
+        return "fail", 400
+    cnx, cursor = get_db()
+    ret = []
+    user_id = session.get('id')
+    try:
+        resume_get_query = "SELECT id FROM resume WHERE user_id=%s"
+        cursor.execute(resume_get_query, (user_id,))
+        for id, in cursor.fetchall():
+            ret.append(id)
+    except:
+        return "fail", 400
+    cnx.close()
+    return ret
+
+@app.route('/api/resume/create', methods=['POST'])
+def resume_create():
+    if not session.get('login'):
+        return "fail", 400
+    text = request.form.get('text')
+    cnx, cursor = get_db()
+    user_id = session.get('id')
+    try:
+        resume_get_query = "INSERT INTO resume (user_id, text) VALUES (%s, %s)"
+        cursor.execute(resume_get_query, (user_id, text))
+        cnx.commit()
+    except:
+        return "fail", 400
+    cnx.close()
+    return "ok"
+
+@app.route('/api/resume/<int:id>/update', methods=['POST'])
+def resume_update(id):
+    if not session.get('login'):
+        return "fail", 400
+    text = request.form.get('text')
+    cnx, cursor = get_db()
+    user_id = session.get('id')
+    try:
+        resume_update_query = "UPDATE resume SET text=%s WHERE id=%s and user_id=%s"
+        cursor.execute(resume_update_query, (text, id, user_id))
+        cnx.commit()
+    except:
+        return "fail", 400
+    if cursor.rowcount != 1:
+        return "fail", 400
+    cnx.close()
+    return "ok"
+
+
+@app.route('/api/resume/<int:id>/delete', methods=['POST'])
+def resume_delete(id):
+    if not session.get('login'):
+        return "fail", 400
+    cnx, cursor = get_db()
+    user_id = session.get('id')
+    try:
+        resume_update_query = "DELETE FROM resume WHERE id=%s and user_id=%s"
+        cursor.execute(resume_update_query, (id, user_id))
+        cnx.commit()
+    except:
+        return "fail", 400
+    if cursor.rowcount != 1:
+        return "fail", 400
+    cnx.close()
+    return "ok"
--- backend/init_db.sql
+++ backend/init_db.sql
@@ -1,3 +1,16 @@
+DROP DATABASE IF EXISTS resume;
 CREATE DATABASE resume;
 USE resume;
-CREATE TABLE users (id int NOT NULL AUTO_INCREMENT, login varchar(255) UNIQUE, password varchar(255), PRIMARY KEY(id));
+CREATE TABLE users (
+    id int NOT NULL AUTO_INCREMENT, 
+    login varchar(255) UNIQUE, 
+    password varchar(255), 
+    PRIMARY KEY(id)
+);
+CREATE TABLE resume (
+    id int NOT NULL AUTO_INCREMENT, 
+    user_id int NOT NULL, 
+    text TEXT,
+    PRIMARY KEY(id), 
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
