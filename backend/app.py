f45c2c7ff88d586125b32ea80cf8fc98d2c6b979
Vladimir Yakovlev <nagos@inbox.ru>
2023-05-25 21:34:24 +0300

    Backend вынесен в отдельный файл

--- backend/app.py
+++ backend/app.py
@@ -15,17 +15,16 @@ session = sessionManager(flask.session)
 
 @app.route('/api/login/register', methods=['POST'])
 def register():
-    create_user_query = "INSERT INTO users (login, password) VALUES (%s, %s)"
     user = request.form.get('user')
     password = request.form.get('password')
         
     try:
-        row, rowid, rowcount = backend.db_query(create_user_query, (user, password))
+        user_id = backend.user_register(user, password)
     except BackendError as err:
         app.logger.error(err)
         return "fail", 400
 
-    session.login(rowid)
+    session.login(user_id)
 
     return "ok"
 
@@ -33,18 +32,13 @@ def register():
 def login():
     user = request.form.get('user')
     password = request.form.get('password')
-    user_password_query = "SELECT id, password FROM users WHERE login=%s"
     try:
-        row, rowid, rowcount = backend.db_query(user_password_query, (user,))
-        if row:
-            user_id, user_password = row[0]
-        else:
-            user_id, user_password = None, None
+        user_id = backend.user_login(user, password)
     except BackendError as err:
         app.logger.error(f'Backend error: {err}')
         return "fail", 400
 
-    if password == user_password:
+    if not user_id is None:
         session.login(user_id)
         return "ok"
     else:
@@ -57,43 +51,40 @@ def logout():
 
 @app.route('/api/resume/<int:id>')
 def resume_get(id):
-    resume_get_query = "SELECT text FROM resume WHERE id=%s"
     try:
-        row, rowid, rowcount = backend.db_query(resume_get_query, (id,))
-        if row:
-            text, = row[0]
-        else:
-            return "fail", 400
+        text = backend.resume_get(id)
     except BackendError as err:
         app.logger.error(f'Backend error: {err}')
         return "fail", 400
-    return text
+
+    if text is None:
+        return "fail", 400
+    else:
+        return text
 
 @app.route('/api/resume/')
 def resume_list():
-    if not s.is_login():
+    if not session.is_login():
         return "fail", 400
-    ret = []
     user_id = session.user_id()
-    resume_get_query = "SELECT id FROM resume WHERE user_id=%s"
     try:
-        row, rowid, rowcount = backend.db_query(resume_get_query, (user_id,))
-        for id, in row:
-            ret.append(id)
+        ids = backend.resume_list(user_id)
     except BackendError as err:
         app.logger.error(f'Backend error: {err}')
         return "fail", 400
-    return ret
+    if len(ids):
+        return ids
+    else:
+        return "fail", 400
 
 @app.route('/api/resume/create', methods=['POST'])
 def resume_create():
-    if not s.is_login():
+    if not session.is_login():
         return "fail", 400
     text = request.form.get('text')
     user_id = session.user_id()
-    resume_get_query = "INSERT INTO resume (user_id, text) VALUES (%s, %s)"
     try:
-        row, rowid, rowcount = backend.db_query(resume_get_query, (user_id, text))
+        backend.resume_create(user_id, text)
     except BackendError as err:
         app.logger.error(f'Backend error: {err}')
         return "fail", 400
@@ -101,31 +92,34 @@ def resume_create():
 
 @app.route('/api/resume/<int:id>/update', methods=['POST'])
 def resume_update(id):
-    if not s.is_login():
+    if not session.is_login():
         return "fail", 400
     text = request.form.get('text')
     user_id = session.user_id()
-    resume_update_query = "UPDATE resume SET text=%s WHERE id=%s and user_id=%s"
+    
     try:
-        row, rowid, rowcount = backend.db_query(resume_update_query, (text, id, user_id))
+        ret = backend.resume_update(user_id, id, text)
     except BackendError as err:
         app.logger.error(f'Backend error: {err}')
         return "fail", 400
-    if rowcount != 1:
+
+    if ret:
+        return "ok"
+    else:
         return "fail", 400
-    return "ok"
 
 @app.route('/api/resume/<int:id>/delete', methods=['POST'])
 def resume_delete(id):
-    if not s.is_login():
+    if not session.is_login():
         return "fail", 400
     user_id = session.user_id()
-    resume_delete_query = "DELETE FROM resume WHERE id=%s and user_id=%s"
     try:
-        row, rowid, rowcount = backend.db_query(resume_delete_query, (id, user_id))
+        ret = backend.resume_delete(user_id, id)
     except BackendError as err:
         app.logger.error(f'Backend error: {err}')
         return "fail", 400
-    if rowcount != 1:
+
+    if ret:
+        return "ok"
+    else:
         return "fail", 400
-    return "ok"
--- backend/backend.py
+++ backend/backend.py
@@ -36,3 +36,54 @@ class Backend():
             cnx.close()
         
         return row, rowid, rowcount
+
+    def user_register(self, user, password):
+        create_user_query = "INSERT INTO users (login, password) VALUES (%s, %s)"
+        row, rowid, rowcount = self.db_query(create_user_query, (user, password))
+        return rowid
+    
+    def user_login(self, user, password):
+        user_password_query = "SELECT id, password FROM users WHERE login=%s"
+        row, rowid, rowcount = self.db_query(user_password_query, (user,))
+        if row:
+            user_id, user_password = row[0]
+        else:
+            user_id, user_password = None, None
+        
+        if password == user_password:
+            return user_id
+        else:
+            return None
+    
+    def resume_list(self, user_id):
+        resume_get_query = "SELECT id FROM resume WHERE user_id=%s"
+        row, rowid, rowcount = self.db_query(resume_get_query, (user_id,))
+        return [id for (id,) in row]
+
+    def resume_get(self, id):
+        resume_get_query = "SELECT text FROM resume WHERE id=%s"
+        row, rowid, rowcount = self.db_query(resume_get_query, (id,))
+        if row:
+            return row[0][0]
+        else:
+            return None
+    
+    def resume_create(self, user_id, text):
+        resume_get_query = "INSERT INTO resume (user_id, text) VALUES (%s, %s)"
+        row, rowid, rowcount = self.db_query(resume_get_query, (user_id, text))
+
+    def resume_update(self, user_id, id, text):
+        resume_update_query = "UPDATE resume SET text=%s WHERE id=%s and user_id=%s"
+        row, rowid, rowcount = self.db_query(resume_update_query, (text, id, user_id))
+        if rowcount == 1:
+            return True
+        else:
+            return False
+    
+    def resume_delete(self, user_id, id):
+        resume_delete_query = "DELETE FROM resume WHERE id=%s and user_id=%s"
+        row, rowid, rowcount = self.db_query(resume_delete_query, (id, user_id))
+        if rowcount == 1:
+            return True
+        else:
+            return False
