98dfa79d3fb24155fcfbb8d9cf69d4b89b584306
Vladimir Yakovlev <nagos@inbox.ru>
2023-05-31 20:20:03 +0300

    Формат запросов login изменен на json

--- backend/app.py
+++ backend/app.py
@@ -16,19 +16,19 @@ session = sessionManager(flask.session)
 
 @app.route('/api/user/register', methods=['POST'])
 def user_register():
-    user = request.form.get('user')
-    password = request.form.get('password')
+    user = request.json.get('user')
+    password = request.json.get('password')
     if user is None or password is None:
-        return "fail", 400
+        return {'login': False}, 400
     try:
         user_id = backend.user_register(user, password)
     except BackendError as err:
         app.logger.error(err)
-        return "fail", 400
+        return {'login': False}, 400
 
     session.login(user_id)
 
-    return "ok"
+    return {'login': True}
 
 @app.route('/api/user/status')
 def user_status():
@@ -39,26 +39,26 @@ def user_status():
 
 @app.route('/api/user/login', methods=['POST'])
 def user_login():
-    user = request.form.get('user')
-    password = request.form.get('password')
+    user = request.json.get('user')
+    password = request.json.get('password')
     if user is None or password is None:
-        return "fail", 400
+        return {'login': False}, 400
     try:
         user_id = backend.user_login(user, password)
     except BackendError as err:
         app.logger.error(f'Backend error: {err}')
-        return "fail", 400
+        return {'login': False}, 400
 
     if not user_id is None:
         session.login(user_id)
-        return "ok"
+        return {'login': True}
     else:
-        return "fail", 400
+        return {'login': False}, 400
 
 @app.route('/api/user/logout', methods=['POST'])
 def user_logout():
     session.logout()
-    return "ok"
+    return {'login': False}
 
 @app.route('/api/resume/<int:id>/<string:fmt>')
 def resume_get(id, fmt):
@@ -66,73 +66,73 @@ def resume_get(id, fmt):
         text = backend.resume_get(id)
     except BackendError as err:
         app.logger.error(f'Backend error: {err}')
-        return "fail", 400
+        return {'status': 'error'}, 400
 
     if text is None:
-        return "fail", 400
+        return {'status': 'error'}, 400
     if fmt == 'html':
         return markdown.markdown(text)
     else:
-        return text
+        return {'text': text}
 
 @app.route('/api/resume/')
 def resume_list():
     if not session.is_login():
-        return "fail", 400
+        return {'status': 'error'}, 400
     user_id = session.user_id()
     try:
         ids = backend.resume_list(user_id)
     except BackendError as err:
         app.logger.error(f'Backend error: {err}')
-        return "fail", 400
+        return {'status': 'error'}, 400
     if len(ids):
         return ids
     else:
-        return "fail", 400
+        return {'status': 'error'}, 400
 
 @app.route('/api/resume/create', methods=['POST'])
 def resume_create():
     if not session.is_login():
-        return "fail", 400
-    text = request.form.get('text')
+        return {'status': 'error'}, 400
+    text = request.json.get('text')
     user_id = session.user_id()
     try:
         backend.resume_create(user_id, text)
     except BackendError as err:
         app.logger.error(f'Backend error: {err}')
-        return "fail", 400
-    return "ok"
+        return {'status': 'error'}, 400
+    return {'status': 'ok'}
 
 @app.route('/api/resume/<int:id>/update', methods=['POST'])
 def resume_update(id):
     if not session.is_login():
-        return "fail", 400
-    text = request.form.get('text')
+        return {'status': 'error'}, 400
+    text = request.json.get('text')
     user_id = session.user_id()
     
     try:
         ret = backend.resume_update(user_id, id, text)
     except BackendError as err:
         app.logger.error(f'Backend error: {err}')
-        return "fail", 400
+        return {'status': 'error'}, 400
 
     if ret:
-        return "ok"
+        return {'status': 'ok'}
     else:
-        return "fail", 400
+        return {'status': 'error'}, 400
 
 @app.route('/api/resume/<int:id>/delete', methods=['POST'])
 def resume_delete(id):
     if not session.is_login():
-        return "fail", 400
+        return {'status': 'error'}, 400
     user_id = session.user_id()
     try:
         ret = backend.resume_delete(user_id, id)
     except BackendError as err:
         app.logger.error(f'Backend error: {err}')
-        return "fail", 400
+        return {'status': 'error'}, 400
 
     if ret:
-        return "ok"
+        return {'status': 'ok'}
     else:
-        return "fail", 400
+        return {'status': 'error'}, 400
